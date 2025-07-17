72409641361d6ec219c15e63739a3fd7f381fc5d
Koresh01 <kmatveev2004@gmail.com>
2025-07-17 15:22:38 +0300

    Revert "Установил библиотеку fmod"
    This reverts commit a6f0ddb99da3fde4f8116e0476c597af4a25524a.

--- Assets/Scripts/Car/Sounds/EngineSound.cs
+++ Assets/Scripts/Car/Sounds/EngineSound.cs
@@ -2,35 +2,15 @@ using UnityEngine;
 
 public class EngineSound : MonoBehaviour
 {
-    [Header("Ссылки")]
-    public CarInput carInput;
-
-    [Header("Настройки BPM")]
-    public float minBPM = 80f; // Холостые обороты
-    public float maxBPM = 522f; // Отсечка
-
-    [Header("Аудио")]
-    public AudioClip engineLoopClip;
-
-    private AudioSource engineSource;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        engineSource = gameObject.AddComponent<AudioSource>();
-        engineSource.clip = engineLoopClip;
-        engineSource.loop = true;
-        engineSource.playOnAwake = true;
-        engineSource.spatialBlend = 1f; // 3D звук
-        engineSource.volume = 1f;
-        engineSource.Play();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float rpm01 = Mathf.Clamp01(carInput.curRPM);
-
-        // Вычисляем текущий BPM и устанавливаем pitch
-        float currentBPM = Mathf.Lerp(minBPM, maxBPM, rpm01);
-        engineSource.pitch = currentBPM / minBPM;
+        
     }
 }
