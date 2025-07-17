7418f779063f271cfd31bb72e3765cd23b711e36
Koresh01 <kmatveev2004@gmail.com>
2025-07-17 07:24:12 +0300

    Изменил отображение текущей передачи.
    Текущая передача теперь отображается на тахометре

--- Assets/Scripts/UI/Gear Listener.cs
+++ Assets/Scripts/UI/Gear Listener.cs
@@ -8,6 +8,13 @@ public class GearListener : MonoBehaviour
 
     void FixedUpdate()
     {
-        curGearUI.text = "Передача: " + (carInput.curGearInx-1);
+        int RealGearInx = carInput.curGearInx - 1;
+
+        if (RealGearInx == -1)
+            curGearUI.text = "R";
+        else if (RealGearInx == 0)
+            curGearUI.text = "N";
+        else
+            curGearUI.text = RealGearInx.ToString();
     }
 }
